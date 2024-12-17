9dd513e4316e90e69448ff7f35f1fddfb8c2b2a3
Aarthi E <aarthielumalaimaha@gmail.com>
2024-12-17 08:55:17 +0530

    Database connected

--- server/server.js
+++ server/server.js
@@ -1,7 +1,29 @@
-const express = require('express')
+const express = require('express');
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('dotenv').config(); // Load environment variables
 
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.json()); 
 
-const app = express()
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,  
+  user: process.env.DB_USER,  
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_DATABASE,  
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL database.");
+});
+
 app.listen(3000, () => {
     console.log('server is running on port 3000')
 })
